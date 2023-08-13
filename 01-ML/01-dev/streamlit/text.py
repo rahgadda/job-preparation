2202ef7662f02962321679f4445737d1dde50b3b
Rahul Kiran Gaddam <gaddam.rahul@gmail.com>
2023-08-13 10:02:46 +0530

    Initial Draft

--- 01-ML/01-dev/streamlit/text.py
+++ 01-ML/01-dev/streamlit/text.py
@@ -13,16 +13,16 @@ st.header("Header")
 # Sub-header
 st.subheader("Sub-header")
 
-# Caption, used as describes notes, footnotes, tables, images, and videos.
+# Caption, used as describes notes, footnotes, tables, images, and videos
 st.caption("Caption")
 
-# Plain Text, it will appear on a new line
+# Plain Text, it will appear on a new line, not data formatting
 st.text("Text")
 
 # Markdown
 st.markdown('# Markdown Header')
 
-# LaTeX is formatted text used for technical documentation.
+# LaTeX is formatted text used for technical documentation
 st.latex("""(a+b)^2 = a^2 + b^2 + 2ab""")
 
 # Code highliter, if language is not added then it will display unformatted
@@ -31,3 +31,12 @@ code = '''def hello():
      print("Hello, Streamlit!")'''
 st.code(code, language='python')
 
+# Displaying Variables, write support display and auto formating multiple data types
+name = "Alice"
+age = 30
+st.write(f"Name: {name}, Age: {age}")
+
+# Metrics, helps the user see any changes in the data easily
+st.metric( label="Population", value="1.6 Billions", delta="1 Billions", delta_color="inverse" )
+st.metric( label="Population", value="1.6 Billions", delta="1 Billions", delta_color="off" )
+st.metric( label="Population", value=None, delta="1 Billions")
