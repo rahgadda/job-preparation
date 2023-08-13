b4498b460931570e137b094e9d8bbf6829f33f06
rahgadda <gaddam.rahul@gmail.com>
2023-08-13 06:52:28 +0000

    Initial Draft

--- 01-ML/01-dev/streamlit/dataframe.py
+++ 01-ML/01-dev/streamlit/dataframe.py
@@ -1,9 +1,12 @@
 import random
 import pandas as pd
 import streamlit as st
+import json
 
+# Title
+st.title("Dataframe Example")
 
-# Dataframe
+# Variables
 df = pd.DataFrame(
     {
         "name": ["Roadmap", "Extras", "Issues"],
@@ -13,6 +16,9 @@ df = pd.DataFrame(
     }
 )
 st.checkbox("Use container width", value=False, key="use_container_width")
+
+# Dataframe
+st.header("Dataframe")
 st.dataframe(
     df,
     column_config={
@@ -28,7 +34,44 @@ st.dataframe(
         ),
     },
     hide_index=True,
-    use_container_width=st.session_state.use_container_widt
+    use_container_width=st.session_state.use_container_width
 )
 
 # Dataframe Editor
+st.header("Dataframe Editor")
+st.data_editor(
+    df,
+    column_config={
+        "name": "App name",
+        "stars": st.column_config.NumberColumn(
+            "Github Stars",
+            help="Number of stars on GitHub",
+            format="%d ⭐",
+        ),
+        "url": st.column_config.LinkColumn("App URL"),
+        "views_history": st.column_config.LineChartColumn(
+            "Views (past 30 days)", y_min=0, y_max=5000
+        ),
+    },
+    hide_index=True,
+    use_container_width=st.session_state.use_container_width,
+    num_rows="dynamic"
+)
+
+# Table
+st.header("Table")
+st.table(df)
+
+# JSON
+st.header("Json")
+body = json.dumps({
+    'foo': 'bar',
+    'baz': 'boz',
+    'stuff': [
+        'stuff 1',
+        'stuff 2',
+        'stuff 3',
+        'stuff 5',
+    ],
+})
+st.json(body, expanded=True)
