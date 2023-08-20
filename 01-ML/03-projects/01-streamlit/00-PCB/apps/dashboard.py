b426f021c3d806b98354a68bf14eee7593664ae8
rahgadda <gaddam.rahul@gmail.com>
2023-08-20 10:13:00 +0000

    Initial Draft

---
+++ 01-ML/03-projects/01-streamlit/00-PCB/apps/dashboard.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def page_content():
+    st.header("Dashboard")
--- 01-ML/03-projects/01-streamlit/00-PCB/demo.py
+++ 01-ML/03-projects/01-streamlit/00-PCB/demo.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import lib.page_settings as page_settings
+import apps.dashboard as dashboard
 
 # -- Page Settings
 page_settings.initialLoad("Telangana Pollution Control Board","Welcome to PCB Command Center!!!")
@@ -8,5 +9,5 @@ page_settings.initialLoad("Telangana Pollution Control Board","Welcome to PCB Co
 # -- Main Menu  
 selected = page_settings.menu(["Dashboard",'Transactions','Setup'],['speedometer','activity', 'gear'])
 
-if selected=="Dashboard":
-    st.title("Dashboard")
+if selected == "Dashboard":
+    dashboard.page_content()
