81cf76d9a55fc9e5054cf3f5a8aa17c699f06312
rahgadda <gaddam.rahul@gmail.com>
2023-08-20 14:22:56 +0000

    Initial Draft

--- 01-ML/03-projects/01-streamlit/00-PCB/lib/page_settings.py
+++ 01-ML/03-projects/01-streamlit/00-PCB/lib/page_settings.py
@@ -10,13 +10,15 @@ def initialLoad(page_title, title_details):
                 </style>
                 """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    st.title(title_details)
+    
+    col1, col2, col3 = st.columns((3,6,1))
+    col2.title(title_details)
 
-def menu(menueList, iconList):
+def menu(menuList, iconList):
     with st.sidebar:
         selected = option_menu(
                                 menu_title=None,
-                                options=menueList,
+                                options=menuList,
                                 icons=iconList, 
                                 menu_icon="cast", 
                                 default_index=0
