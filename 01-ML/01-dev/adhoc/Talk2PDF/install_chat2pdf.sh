e4915587ad28aa818732ec24574636a01bbfe69c
rahgadda <gaddam.rahul@gmail.com>
2024-03-13 17:54:19 +0000

    Initial Draft

--- 01-ML/01-dev/adhoc/Talk2PDF/install_chat2pdf.sh
+++ 01-ML/01-dev/adhoc/Talk2PDF/install_chat2pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install streamlit pypdf bitsandbytes loralib accelerate torch faiss-cpu numpy langchain_community sentence-transformers langchain git+https://github.com/huggingface/transformers
+pip install streamlit pypdf
 streamlit run ./Chat2PDF.py
