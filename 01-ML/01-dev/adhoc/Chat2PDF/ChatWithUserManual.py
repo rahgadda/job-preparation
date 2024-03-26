47acc9e2d3a2f06bbcaa5028b38ec2811846dfc3
rahgadda <gaddam.rahul@gmail.com>
2024-03-26 06:32:15 +0000

    Initial Draft

---
+++ 01-ML/01-dev/adhoc/Chat2PDF/ChatWithUserManual.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from dotenv import load_dotenv
+
+# Loading Google Gemini API Key
+load_dotenv()
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
---
+++ 01-ML/01-dev/adhoc/Chat2PDF/install_chatwithum.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install streamlit pypdf langchain-google-genai google.generativeai python-dotenv langchain langchain_community 
+streamlit run ./ChatWithUserManual.py
