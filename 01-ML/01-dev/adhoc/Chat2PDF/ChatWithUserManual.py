64d5f0b217fb981300b191155636dc225bf0150d
rahgadda <gaddam.rahul@gmail.com>
2024-03-26 07:07:20 +0000

    Initial Draft

--- 01-ML/01-dev/adhoc/Chat2PDF/ChatWithUserManual.py
+++ 01-ML/01-dev/adhoc/Chat2PDF/ChatWithUserManual.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import google.generativeai as genai
 from dotenv import load_dotenv
 
-# Loading Google Gemini API Key
+# Loading Google Gemini API Key from Environment Variables
 load_dotenv()
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+
+# Main Program
+def main():
+    st.set_page_config("Chat With Your Product User Manual")
+    st.header("Chat With Your Product User Manual💁")
+
+
+# Loading Main
+if __name__ == "__main__":
+    main()
