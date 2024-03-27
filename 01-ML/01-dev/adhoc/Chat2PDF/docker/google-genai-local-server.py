14518b860fb0990f27e8decfcfffafbc3b9c5b5d
rahgadda <gaddam.rahul@gmail.com>
2024-03-27 07:32:32 +0000

    Initial Draft

--- 01-ML/01-dev/adhoc/Chat2PDF/docker/google-genai-local-server.py
+++ 01-ML/01-dev/adhoc/Chat2PDF/docker/google-genai-local-server.py
@@ -10,18 +10,6 @@ from langchain_community.document_loaders import TextLoader
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def root():
-    logs = request.args.get('logs')
-
-    if logs:
-        # Return "Building in progress" message in HTML format
-        return "<h1>Building in progress</h1>", 200, {'Content-Type': 'text/html'}
-    else:
-        # Return "Search at /generate_output" message in HTML format
-        return "<h1>Search at <a href='/api'>/api</a></h1>", 200, {'Content-Type': 'text/html'}
-
-
-@app.route('/api', methods=['GET'])
 def api():
 
     # Loading Google Gemini API Key from Environment Variables
@@ -31,41 +19,45 @@ def api():
     # Getting the input string from the request
     input_string = request.args.get('query')
 
-    # Variables
-    lv_template   = """Instruction:
-                    You are an AI assistant for answering questions about the provided context.
-                    You are given the following extracted parts of a long document and a question. Provide a detailed answer.
-                    If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-                    =======
-                    {context}
-                    =======
-                    Question: {question}
-                    Output:\n"""
+    if input_string:
+        # Variables
+        lv_template   = """Instruction:
+                        You are an AI assistant for answering questions about the provided context.
+                        You are given the following extracted parts of a long document and a question. Provide a detailed answer.
+                        If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
+                        =======
+                        {context}
+                        =======
+                        Question: {question}
+                        Output:\n"""
 
-    lv_qa_prompt = PromptTemplate(
-                                    template=lv_template,
-                                    input_variables=["question", "context"]
-                                 ) 
-    
-    lv_model = genai.GenerativeModel('gemini-pro')
+        lv_qa_prompt = PromptTemplate(
+                                        template=lv_template,
+                                        input_variables=["question", "context"]
+                                    ) 
+        
+        lv_model = genai.GenerativeModel('gemini-pro')
 
-    lv_file_name = "oracle-banking-common-core-user-guide-pages-5.txt"
-    lv_temp_file_path = os.path.join(os.path.join("vectordb","txt"),lv_file_name)
-    lv_text_loader = TextLoader(lv_temp_file_path)
-    lv_pdf_formatted_content = lv_text_loader.load()
-    lv_text_data = ""    
-    for lv_page in lv_pdf_formatted_content:
-        lv_text_data = lv_text_data + lv_page.page_content
+        lv_file_name = "oracle-banking-common-core-user-guide-pages-5.txt"
+        lv_temp_file_path = os.path.join(os.path.join("vectordb","txt"),lv_file_name)
+        lv_text_loader = TextLoader(lv_temp_file_path)
+        lv_pdf_formatted_content = lv_text_loader.load()
+        lv_text_data = ""    
+        for lv_page in lv_pdf_formatted_content:
+            lv_text_data = lv_text_data + lv_page.page_content
 
-    lv_qa_formatted_prompt = lv_qa_prompt.format(  
-                                                    question=input_string,
-                                                    context=lv_text_data
-                                                )
+        lv_qa_formatted_prompt = lv_qa_prompt.format(  
+                                                        question=input_string,
+                                                        context=lv_text_data
+                                                    )
 
-    lv_llm_response = lv_model.generate_content(lv_qa_formatted_prompt).text
+        lv_llm_response = lv_model.generate_content(lv_qa_formatted_prompt).text
 
-    # Returning the output as JSON response
-    return jsonify({'response': lv_llm_response})
+        # Returning the output as JSON response
+        return jsonify({'response': lv_llm_response}), 200, {'Content-Type': 'application/json'}
+    else:
+        # Return "Search at /generate_output" message in HTML format
+        return "<h1>Add Query Parameter</h1>", 200, {'Content-Type': 'text/html'}
 
 
 if __name__ == '__main__':
