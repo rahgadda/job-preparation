e64acaa077f7be22dead9dc833045a878e62c933
rahgadda <gaddam.rahul@gmail.com>
2024-03-27 06:55:09 +0000

    Initial Draft

--- 01-ML/01-dev/adhoc/Chat2PDF/docker/create_docker.sh
+++ 01-ML/01-dev/adhoc/Chat2PDF/docker/create_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -t google-genai-local-server .
-# docker run -p 7860:7860 -e GOOGLE_API_KEY=--- google-genai-local-server
+docker run -p 7860:7860 -e GOOGLE_API_KEY=--- google-genai-local-server
 docker tag google-genai-local-server rahgadda/google-genai-local-server:latest
 docker push rahgadda/google-genai-local-server:latest
